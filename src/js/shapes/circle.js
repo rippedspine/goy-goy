b0d1bd75403aa647e67302d657b0f2901417acf9
rippedspine <gabriella.karin@gmail.com>
2014-11-08 14:29:48 +0100

    Refactor circle.js, Change bower.json

--- src/js/shapes/circle.js
+++ src/js/shapes/circle.js
@@ -10,10 +10,10 @@
   };
   inherits(Circle, BaseShape);
 
-  Circle.prototype.draw = function(context) {
-    this.beginDraw(context);
-    context.arc(0, 0, this.radius, 0, 2 * utils.myMath.PI, false);
-    this.endDraw(context);
+  Circle.prototype.draw = function() {
+    this.beginDraw();
+    BaseShape.context.arc(0, 0, this.radius, 0, 2 * utils.myMath.PI, false);
+    this.endDraw();
   };
 
   module.exports = Circle;
